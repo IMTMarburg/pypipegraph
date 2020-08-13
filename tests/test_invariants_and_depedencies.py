bcda8c9e8ff5f63b45768f9a20ea2f9a24561fb2
Unknown <finkernagel@imt.uni-marburg.de>
2020-08-13 16:48:35 +0200

    cython function bitrot fixed and test cases for cython get source

--- tests/test_invariants_and_depedencies.py
+++ tests/test_invariants_and_depedencies.py
@@ -947,6 +947,7 @@ class TestFunctionInvariant:
 
     def test_cython_function(self):
         # horrible mocking hack to see that it actually extracts something, - not tested if it's the right thing...
+        # probably unnecssary now that we have TestCythonCompability
         import stat
 
         class MockImClass:
@@ -1979,3 +1980,82 @@ RETURN_VALUE"""
         assert a.get_invariant(False, []) == b.get_invariant(False, [])
         assert a.get_invariant(False, []) != c.get_invariant(False, [])
         assert c.get_invariant(False, []) == d.get_invariant(False, [])
+
+
+@pytest.mark.usefixtures("new_pipegraph")
+class TestCythonCompability():
+
+
+    def test_just_a_function(self):
+        import cython
+
+        src ="""
+def a():
+    return 1
+
+def b():
+    return 5
+"""
+        func = cython.inline(src)['a']
+        actual = ppg.FunctionInvariant('a',func).get_invariant(None, {})
+        should = """    def a():
+        return 1
+    """
+        assert actual == should
+
+    def test_nested_function(self):
+        import cython
+
+        src ="""
+def a():
+    def b():
+        return 1
+    return b
+
+def c():
+    return 5
+"""
+        func = cython.inline(src)['a']()
+        actual = ppg.FunctionInvariant('a',func).get_invariant(None, {})
+        should = """        def b():
+            return 1"""
+        assert actual == should
+
+    def test_class(self):
+        import cython
+
+        src ="""
+class A():
+    def b(self):
+        return 55
+
+def c():
+    return 5
+"""
+        import inspect
+        func = cython.inline(src)['A']().b
+        actual = ppg.FunctionInvariant('a',func).get_invariant(None, {})
+        should = """        def b(self):
+            return 55
+    """
+        assert actual == should
+
+    def test_class_inner_function(self):
+        import cython
+
+        src ="""
+class A():
+    def b(self):
+        def c():
+            return 55
+        return c
+
+def d():
+    return 5
+"""
+        import inspect
+        func = cython.inline(src)['A']().b()
+        actual = ppg.FunctionInvariant('a',func).get_invariant(None, {})
+        should = """            def c():
+                return 55"""
+        assert actual == should
